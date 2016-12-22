71be677a3ef7c188cd78ccd55808e6dd1b0a5af1
Johan Beyers <johan@juizi.com>
2016-12-22 10:41:00 +0200

    Uppercase layouts

--- contentblocks/constants.py
+++ contentblocks/constants.py
@@ -4,10 +4,10 @@ from django.conf import settings
 infoblock_layouts = [("detail", "Detail view")]
 
 
-if hasattr(settings, "CONTENTBLOCKS") and "layouts" in settings.CONTENTBLOCKS:
-    layout_names = [i[0] for i in settings.CONTENTBLOCKS["layouts"]]
+if hasattr(settings, "CONTENTBLOCKS") and "LAYOUTS" in settings.CONTENTBLOCKS:
+    layout_names = [i[0] for i in settings.CONTENTBLOCKS["LAYOUTS"]]
     if "detail" in layout_names:
-        infoblock_layouts = settings.CONTENTBLOCKS["layouts"][:]
+        infoblock_layouts = settings.CONTENTBLOCKS["LAYOUTS"][:]
     else:
-        infoblock_layouts.extend(settings.CONTENTBLOCKS["layouts"][:])
+        infoblock_layouts.extend(settings.CONTENTBLOCKS["LAYOUTS"][:])
 
